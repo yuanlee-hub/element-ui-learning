45abba43249c3f6cc87023951482a1dac2852af9
liyuan <liyuan520310@163.com>
2019-10-13 21:03:01 +0800

    Add component Badge

--- src/components/badge/src/main.vue
+++ src/components/badge/src/main.vue
@@ -1,26 +1,46 @@
 <template>
-  <div aria="Badge"></div>
+  <div aria="Badge" class="el-badge">
+    <slot></slot>
+    <sup :class="classList" v-show="value!==void 0 && !hidden">{{ content }}</sup>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Cl' + 'Badge',
   props: {
-
-  },
-  data () {
-    return {
-
+    value: {
+      type: [String, Number]
+    },
+    max: Number,
+    isDot: {
+      type: Boolean,
+      default: false
+    },
+    hidden: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String
     }
   },
   computed: {
-
-  },
-  methods: {
-
-  },
-  created () {
-
+    classList () {
+      const list = ['el-badge__content', 'is-fixed', this.type ? 'el-badge__content--' + this.type : '']
+      list.push({
+        'is-dot': this.isDot
+      })
+      return list
+    },
+    content () {
+      if (this.isDot) return
+      const { value, max } = this
+      if (typeof value === 'number' && typeof max === 'number') {
+        return value > max ? `${max}+` : value
+      }
+      return value
+    }
   }
 }
 </script>
--- src/views/badge/index.vue
+++ src/views/badge/index.vue
@@ -2,21 +2,149 @@
   <div class="demo">
     <dl>
       <dt>Demo</dt>
-      <dd></dd>
+      <dd>
+        <dl>
+          <dt>base</dt>
+          <dd>
+            <el-badge>
+              <el-button>no value</el-button>
+            </el-badge>
+
+            <el-badge value="0">
+              <el-button>0</el-button>
+            </el-badge>
+
+            <el-badge :value="10">
+              <el-button>评论</el-button>
+            </el-badge>
+
+            <el-badge :value="10" is-dot>
+              <el-button>dot</el-button>
+            </el-badge>
+
+            <el-badge :value="10" hidden>
+              <el-button>hidden</el-button>
+            </el-badge>
+          </dd>
+        </dl>
+
+        <dl>
+          <dt>type</dt>
+          <dd>
+            <el-badge :value="10">
+              <el-button>normal</el-button>
+            </el-badge>
+            <el-badge :value="10" type="primary">
+              <el-button>primary</el-button>
+            </el-badge>
+            <el-badge :value="10" type="success">
+              <el-button>评论</el-button>
+            </el-badge>
+            <el-badge :value="10" type="danger">
+              <el-button>评论</el-button>
+            </el-badge>
+
+          </dd>
+        </dl>
+
+        <dl>
+          <dt>max</dt>
+          <dd>
+            <el-badge :value="9" :max="2">
+              <el-button>评论</el-button>
+            </el-badge>
+
+            <el-badge :value="9" :max="0">
+              <el-button>评论</el-button>
+            </el-badge>
+          </dd>
+        </dl>
+
+
+      </dd>
     </dl>
 
     <dl>
       <dt>DEV</dt>
-      <dd></dd>
+      <dd>
+        <dl>
+          <dt>base</dt>
+          <dd>
+            <cl-badge>
+              <cl-button>no value</cl-button>
+            </cl-badge>
+
+            <cl-badge value="0">
+              <cl-button>0</cl-button>
+            </cl-badge>
+
+            <cl-badge :value="10">
+              <cl-button>评论</cl-button>
+            </cl-badge>
+
+            <cl-badge :value="10" is-dot>
+              <cl-button>dot</cl-button>
+            </cl-badge>
+
+            <cl-badge :value="10" hidden>
+              <cl-button>hidden</cl-button>
+            </cl-badge>
+          </dd>
+        </dl>
+
+        <dl>
+          <dt>type</dt>
+          <dd>
+            <cl-badge :value="10">
+              <cl-button>normal</cl-button>
+            </cl-badge>
+            <cl-badge :value="10" type="primary">
+              <cl-button>primary</cl-button>
+            </cl-badge>
+            <cl-badge :value="10" type="success">
+              <cl-button>评论</cl-button>
+            </cl-badge>
+            <cl-badge :value="10" type="danger">
+              <cl-button>评论</cl-button>
+            </cl-badge>
+
+          </dd>
+        </dl>
+
+        <dl>
+          <dt>max</dt>
+          <dd>
+            <cl-badge :value="200" :max="100">
+              <cl-button>评论</cl-button>
+            </cl-badge>
+
+            <cl-badge :value="9" :max="2">
+              <cl-button>评论</cl-button>
+            </cl-badge>
+
+            <cl-badge :value="9" :max="0">
+              <cl-button>评论</cl-button>
+            </cl-badge>
+          </dd>
+        </dl>
+
+      </dd>
     </dl>
 
   </div>
 </template>
 
 <script>
+import ClButton from '@/components/Button'
 import ClBadge from '@/components/Badge'
 export default {
   name: 'Badge' + 'Demo',
-  components: { ClBadge }
+  components: { ClButton, ClBadge }
 }
 </script>
+
+<style scoped>
+  dd > div {
+    margin-right: 30px;
+  }
+</style>
